CraftMyPDF/n8n-nodes-craftmypdf

commit c00eaa25203ceb9e65ab69b477734ae4ac9c56fc

feat: new CraftMyPdf node
Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

nodes/CraftMyPdf/CraftMyPdf.node.ts
@@ -1,0 +1,280 @@
+import {
+	type IDataObject,
+	type IExecuteFunctions,
+	type INodeExecutionData,
+	type INodeType,
+	type INodeTypeDescription,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+import { accountOperations } from './descriptions/AccountDescription';
+import { imageFields, imageOperations } from './descriptions/ImageDescription';
+import { pdfFields, pdfOperations } from './descriptions/PdfDescription';
+import { transactionFields, transactionOperations } from './descriptions/TransactionDescription';
+import { craftMyPdfApiRequest, returnFileExportType, validateJSON } from './GenericFunctions';
+
+export class CraftMyPdf implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'CraftMyPdf',
+		name: 'craftMyPdf',
+		icon: 'file:craftMyPdf.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
+		description: 'Consume the CraftMyPDF API',
+		defaults: {
+			name: 'CraftMyPDF',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'craftMyPdfApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Account',
+						value: 'account',
+					},
+					{
+						name: 'Image',
+						value: 'image',
+					},
+					{
+						name: 'PDF',
+						value: 'pdf',
+					},
+					{
+						name: 'Transaction',
+						value: 'transaction',
+					},
+				],
+				default: 'account',
+			},
+
+			...accountOperations,
+
+			...imageOperations,
+			...imageFields,
+
+			...pdfOperations,
+			...pdfFields,
+
+			...transactionOperations,
+			...transactionFields,
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][] | null> {
+		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
+		const length = items.length;
+		let responseData;
+
+		const resource = this.getNodeParameter('resource', 0);
+		const operation = this.getNodeParameter('operation', 0);
+
+		for (let i = 0; i < length; i++) {
+			try {
+				if (resource === 'account') {
+					if (operation === 'get') {
+						// Account Management API: Get account info
+						// https://craftmypdf.com/docs/index.html#tag/Account-Management-API/operation/get-account-info
+						responseData = await craftMyPdfApiRequest.call(this, 'GET', '/get-account-info');
+
+						returnData.push(responseData as INodeExecutionData);
+					}
+				}
+				if (resource === 'image') {
+					if (operation === 'create') {
+						// Image Generation API: Create an image
+						// https://craftmypdf.com/docs/index.html#tag/Image-Generation-API/operation/create-image
+						const data = validateJSON(this.getNodeParameter('data', i) as string);
+						if (data === undefined) {
+							throw new NodeOperationError(this.getNode(), 'Data: Invalid JSON', {
+								itemIndex: i,
+							});
+						}
+						const export_type = this.getNodeParameter('export_type', i) as string;
+						const output_file = this.getNodeParameter('output_file', i) as string;
+
+						const body = {
+							template_id: this.getNodeParameter('templateId', i) as string,
+							data,
+							load_data_from: this.getNodeParameter('load_data_from', i) as string,
+							version: this.getNodeParameter('version', i) as string,
+							export_type,
+							expiration: this.getNodeParameter('expiration', i) as string,
+							output_file,
+							output_type: this.getNodeParameter('output_type', i) as string,
+						};
+
+						if (export_type === 'json') {
+							responseData = await craftMyPdfApiRequest.call(
+								this,
+								'POST',
+								'/create-image',
+								{},
+								body,
+							);
+							returnData.push(responseData as INodeExecutionData);
+						}
+						if (export_type === 'file') {
+							const binaryData = await returnFileExportType.call(
+								this,
+								'POST',
+								'/create-image',
+								output_file,
+								body,
+							);
+							returnData.push(binaryData);
+						}
+					}
+				}
+				if (resource === 'pdf') {
+					if (operation === 'create') {
+						// PDF Generation API: Create a PDF
+						// https://craftmypdf.com/docs/index.html#tag/PDF-Generation-API/operation/create
+						const data = validateJSON(this.getNodeParameter('data', i) as string);
+						if (data === undefined) {
+							throw new NodeOperationError(this.getNode(), 'Data: Invalid JSON', {
+								itemIndex: i,
+							});
+						}
+						const export_type = this.getNodeParameter('export_type', i) as string;
+						const output_file = this.getNodeParameter('output_file', i) as string;
+						const password_protected = this.getNodeParameter('password_protected', i) as boolean;
+						const resize_images = this.getNodeParameter('resize_images', i) as boolean;
+
+						const body: IDataObject = {
+							template_id: this.getNodeParameter('templateId', i) as string,
+							data,
+							load_data_from: this.getNodeParameter('load_data_from', i) as string,
+							version: this.getNodeParameter('version', i) as string,
+							export_type,
+							expiration: this.getNodeParameter('expiration', i) as string,
+							output_file,
+							password_protected,
+							image_resample_res: this.getNodeParameter('image_resample_res', i) as string,
+							resize_images,
+						};
+						if (password_protected) {
+							body.password = this.getNodeParameter('password', i) as string;
+						}
+						if (resize_images) {
+							body.resize_max_width = this.getNodeParameter('resize_max_width', i) as string;
+							body.resize_max_height = this.getNodeParameter('resize_max_height', i) as string;
+							body.resize_format = this.getNodeParameter('resize_format', i) as string;
+						}
+
+						if (export_type === 'json') {
+							responseData = await craftMyPdfApiRequest.call(this, 'POST', '/create', {}, body);
+							returnData.push(responseData as INodeExecutionData);
+						}
+						if (export_type === 'file') {
+							const binaryData = await returnFileExportType.call(
+								this,
+								'POST',
+								'/create',
+								output_file,
+								body,
+							);
+							returnData.push(binaryData);
+						}
+					}
+					if (operation === 'createAsync') {
+						// PDF Generation API: Create a PDF asynchronously
+						// https://craftmypdf.com/docs/index.html#tag/PDF-Generation-API/operation/create-async
+						const data = validateJSON(this.getNodeParameter('data', i) as string);
+						if (data === undefined) {
+							throw new NodeOperationError(this.getNode(), 'Data: Invalid JSON', {
+								itemIndex: i,
+							});
+						}
+						const resize_images = this.getNodeParameter('resize_images', i) as boolean;
+
+						const body: IDataObject = {
+							template_id: this.getNodeParameter('templateId', i) as string,
+							data,
+							version: this.getNodeParameter('version', i) as string,
+							expiration: this.getNodeParameter('expiration', i) as string,
+							webhook_url: this.getNodeParameter('webhook_url', i) as string,
+							image_resample_res: this.getNodeParameter('image_resample_res', i) as string,
+							resize_images,
+						};
+						if (resize_images) {
+							body.resize_max_width = this.getNodeParameter('resize_max_width', i) as string;
+							body.resize_max_height = this.getNodeParameter('resize_max_height', i) as string;
+							body.resize_format = this.getNodeParameter('resize_format', i) as string;
+						}
+
+						responseData = await craftMyPdfApiRequest.call(this, 'POST', '/create-async', {}, body);
+						returnData.push(responseData as INodeExecutionData);
+					}
+					if (operation === 'merge') {
+						// PDF Manipulation API: Merge PDF URLs
+						// https://craftmypdf.com/docs/index.html#tag/PDF-Manipulation-API/operation/merge-pdfs
+						const urls = this.getNodeParameter('urls', i) as IDataObject[];
+
+						const body: IDataObject = {
+							urls: urls.map((data) => data.url),
+							expiration: this.getNodeParameter('expiration', i) as string,
+							output_file: this.getNodeParameter('output_file', i) as string,
+						};
+
+						responseData = await craftMyPdfApiRequest.call(this, 'POST', '/merge-pdfs', {}, body);
+						returnData.push(responseData as INodeExecutionData);
+					}
+					if (operation === 'addWatermark') {
+						// PDF Manipulation API: Add watermark to a PDF
+						// https://craftmypdf.com/docs/index.html#tag/PDF-Manipulation-API/operation/add-watermark
+						const body: IDataObject = {
+							url: this.getNodeParameter('url', i) as string,
+							text: this.getNodeParameter('text', i) as string,
+							font_size: this.getNodeParameter('font_size', i) as string,
+							opacity: this.getNodeParameter('opacity', i) as string,
+							rotation: this.getNodeParameter('rotation', i) as string,
+							hex_color: this.getNodeParameter('hex_color', i) as string,
+							font_family: this.getNodeParameter('font_family', i) as string,
+							expiration: this.getNodeParameter('expiration', i) as string,
+							output_file: this.getNodeParameter('output_file', i) as string,
+						};
+
+						responseData = await craftMyPdfApiRequest.call(
+							this,
+							'POST',
+							'/add-watermark',
+							{},
+							body,
+						);
+						returnData.push(responseData as INodeExecutionData);
+					}
+				}
+				if (resource === 'transaction') {
+					if (operation === 'list') {
+						// Account Management API: List transactions
+						// https://craftmypdf.com/docs/index.html#tag/Account-Management-API/operation/list-transactions
+						responseData = await craftMyPdfApiRequest.call(this, 'GET', '/list-transactions');
+
+						returnData.push(responseData as INodeExecutionData);
+					}
+				}
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({ error: error.message, json: {} });
+					continue;
+				}
+				throw error;
+			}
+		}
+		return [this.helpers.returnJsonArray(returnData)];
+	}
+}

credentials/CraftMyPdfApi.credentials.ts
@@ -1,0 +1,40 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class CraftMyPdfApi implements ICredentialType {
+	name = 'craftMyPdfApi';
+
+	displayName = 'CraftMyPDF API';
+
+	documentationUrl = 'https://craftmypdf.com/docs/index.html#section/Authentication';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'X-API-KEY': '={{$credentials.apiKey}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.craftmypdf.com/v1',
+			url: '/get-account-info',
+		},
+	};
+}

credentials/ExampleCredentialsApi.credentials.ts
@@ -1,56 +1,0 @@
-import {
-	IAuthenticateGeneric,
-	ICredentialTestRequest,
-	ICredentialType,
-	INodeProperties,
-} from 'n8n-workflow';
-
-export class ExampleCredentialsApi implements ICredentialType {
-	name = 'exampleCredentialsApi';
-	displayName = 'Example Credentials API';
-	properties: INodeProperties[] = [
-		// The credentials to get from user and save encrypted.
-		// Properties can be defined exactly in the same way
-		// as node properties.
-		{
-			displayName: 'User Name',
-			name: 'username',
-			type: 'string',
-			default: '',
-		},
-		{
-			displayName: 'Password',
-			name: 'password',
-			type: 'string',
-			typeOptions: {
-				password: true,
-			},
-			default: '',
-		},
-	];
-
-	// This credential is currently not used by any node directly
-	// but the HTTP Request node can use it to make requests.
-	// The credential is also testable due to the `test` property below
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			auth: {
-				username: '={{ $credentials.username }}',
-				password: '={{ $credentials.password }}',
-			},
-			qs: {
-				// Send this as part of the query string
-				n8n: 'rocks',
-			},
-		},
-	};
-
-	// The block below tells how this credential can be tested
-	test: ICredentialTestRequest = {
-		request: {
-			baseURL: 'https://example.com/',
-			url: '',
-		},
-	};
-}

credentials/HttpBinApi.credentials.ts
@@ -1,50 +1,0 @@
-import {
-	IAuthenticateGeneric,
-	ICredentialTestRequest,
-	ICredentialType,
-	INodeProperties,
-} from 'n8n-workflow';
-
-export class HttpBinApi implements ICredentialType {
-	name = 'httpbinApi';
-	displayName = 'HttpBin API';
-	documentationUrl = '<your-docs-url>';
-	properties: INodeProperties[] = [
-		{
-			displayName: 'Token',
-			name: 'token',
-			type: 'string',
-			default: '',
-			typeOptions: {
-				password: true,
-			}
-		},
-		{
-			displayName: 'Domain',
-			name: 'domain',
-			type: 'string',
-			default: 'https://httpbin.org',
-		},
-	];
-
-	// This allows the credential to be used by other parts of n8n
-	// stating how this credential is injected as part of the request
-	// An example is the Http Request node that can make generic calls
-	// reusing this credential
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			headers: {
-				Authorization: '={{"Bearer " + $credentials.token}}',
-			},
-		},
-	};
-
-	// The block below tells how this credential can be tested
-	test: ICredentialTestRequest = {
-		request: {
-			baseURL: '={{$credentials?.domain}}',
-			url: '/bearer',
-		},
-	};
-}

nodes/CraftMyPdf/GenericFunctions.ts
@@ -1,0 +1,93 @@
+import type {
+	IExecuteFunctions,
+	ILoadOptionsFunctions,
+	JsonObject,
+	IRequestOptions,
+	IHttpRequestMethods,
+	IDataObject,
+	INodeExecutionData,
+} from 'n8n-workflow';
+import { NodeApiError } from 'n8n-workflow';
+
+export async function craftMyPdfApiRequest(
+	this: IExecuteFunctions | ILoadOptionsFunctions,
+	method: IHttpRequestMethods,
+	endpoint: string,
+	qs = {},
+	body = {},
+	option: IDataObject = {},
+) {
+	let options: IRequestOptions = {
+		headers: {
+			'user-agent': 'n8n',
+			Accept: 'application/json',
+		},
+		uri: `https://api.craftmypdf.com/v1${endpoint}`,
+		method,
+		qs,
+		body,
+		followRedirect: true,
+		followAllRedirects: true,
+		json: true,
+	};
+
+	if (Object.keys(option).length !== 0) {
+		options = Object.assign({}, options, option);
+	}
+
+	if (!Object.keys(body).length) {
+		delete options.body;
+	}
+
+	if (!Object.keys(qs).length) {
+		delete options.qs;
+	}
+
+	try {
+		const response = await this.helpers.requestWithAuthentication.call(
+			this,
+			'craftMyPdfApi',
+			options,
+		);
+		if (response.status === 'error') {
+			throw new NodeApiError(this.getNode(), response.message as JsonObject);
+		}
+		return response;
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error as JsonObject);
+	}
+}
+
+export function validateJSON(json: string | undefined): any {
+	let result;
+	try {
+		result = JSON.parse(json!);
+	} catch (exception) {
+		result = undefined;
+	}
+	return result;
+}
+
+export async function returnFileExportType(
+	this: IExecuteFunctions,
+	method: IHttpRequestMethods,
+	endpoint: string,
+	outputFile: string,
+	body = {},
+): Promise<INodeExecutionData> {
+	const responseData = await craftMyPdfApiRequest.call(this, method, endpoint, {}, body, {
+		useStream: true,
+		resolveWithFullResponse: true,
+		encoding: null,
+		json: false,
+	});
+
+	const binaryData = await this.helpers.prepareBinaryData(responseData.body, outputFile);
+
+	return {
+		json: {},
+		binary: {
+			[outputFile]: binaryData,
+		},
+	};
+}

nodes/CraftMyPdf/craftMyPdf.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 20 31"><path fill="#fc384c" d="M7.7 1.5V30l.1.3.2.3.4.1.3-.1L18 24l.4-.4.4-.5.2-.5.1-.6.2-13.7-.1-.6q0-.3-.2-.5l-.3-.5-.5-.4L8.8.7 8.4.6l-.3.2-.2.2-.1.4z"/><path fill="#fc384c" d="M3.7 3.8 3.6 29l.1.2.1.2h.3l.2-.1 1.8-2 .3-.4.2-.5.2-.5V6l-.1-.5-.1-.5-.3-.4-.4-.3-1.4-.9h-.3l-.2.1-.2.1-.1.3z" opacity=".8"/><path fill="#ff2e00" d="M.4 5.6v21l.1.2.1.1.2.1.2-.1 1.1-.6.3-.2.2-.3.1-.3.1-.4V7l-.1-.4-.1-.3-.2-.3-.3-.2-1-.6-.2-.1H.6l-.1.2-.1.2z" opacity=".4"/></svg>

nodes/CraftMyPdf/descriptions/AccountDescription.ts
@@ -1,0 +1,25 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const accountOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['account'],
+			},
+		},
+		options: [
+			// https://craftmypdf.com/docs/index.html#tag/Account-Management-API/operation/get-account-info
+			{
+				name: 'Get',
+				value: 'get',
+				description: 'Get account information',
+				action: 'Get account information',
+			},
+		],
+		default: 'get',
+	},
+];

nodes/CraftMyPdf/descriptions/ImageDescription.ts
@@ -1,0 +1,169 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const imageOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['image'],
+			},
+		},
+		options: [
+			// https://craftmypdf.com/docs/index.html#tag/Image-Generation-API/operation/create-image
+			{
+				name: 'Create',
+				value: 'create',
+				description: 'Creates an image file with JSON data and your template',
+				action: 'Create an image',
+			},
+		],
+		default: 'create',
+	},
+];
+
+export const imageFields: INodeProperties[] = [
+	// ----------------------------------
+	// image:create
+	// https://craftmypdf.com/docs/index.html#tag/Image-Generation-API/operation/create-image
+	// ----------------------------------
+	{
+		displayName: 'Template ID',
+		name: 'templateId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Data',
+		name: 'data',
+		type: 'json',
+		default: '{}',
+		description: 'JSON data, it supports stringified JSON string or an JSON object',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Load Data From',
+		name: 'load_data_from',
+		type: 'string',
+		default: '',
+		description:
+			'Load data from an external URL. If this is specified, it will overwrite the `data` property.',
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Version',
+		name: 'version',
+		type: 'string',
+		default: '',
+		description:
+			'To generate an image using a specific version of the template. We use the latest template version unless you specify otherwise.',
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Export Type',
+		name: 'export_type',
+		type: 'options',
+		default: 'json',
+		description: 'Export type of the image',
+		required: true,
+		options: [
+			{
+				name: 'JSON',
+				value: 'json',
+				description:
+					'Returns a JSON object, and the output image is stored on a CDN (until expiration)',
+			},
+			{
+				name: 'File',
+				value: 'file',
+				description:
+					'Returns binary data of the generated image (More secure and completely private) and the response HTTP header Content-Disposition is set to attachment',
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Expiration',
+		name: 'expiration',
+		type: 'number',
+		typeOptions: {
+			minValue: 1,
+			maxValue: 10080,
+		},
+		default: 5,
+		description: 'Expiration of the generated image in minutes',
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Output File',
+		name: 'output_file',
+		type: 'string',
+		default: 'output.jpg',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Output Type',
+		name: 'output_type',
+		type: 'options',
+		default: 'jpeg',
+		description: 'Image output file type',
+		required: true,
+		options: [
+			{
+				name: 'JPEG',
+				value: 'jpeg',
+			},
+			{
+				name: 'PNG',
+				value: 'png',
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: ['image'],
+				operation: ['create'],
+			},
+		},
+	},
+];

commit 0197b5ec4d14dfa9cbe0c0fad47031665a35e796

fix: add query string GET /list-transactions
Fixes: c00eaa2 ("feat: new CraftMyPdf node")
Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

nodes/CraftMyPdf/CraftMyPdf.node.ts
@@ -262,7 +262,12 @@ export class CraftMyPdf implements INodeType {
 					if (operation === 'list') {
 						// Account Management API: List transactions
 						// https://craftmypdf.com/docs/index.html#tag/Account-Management-API/operation/list-transactions
-						responseData = await craftMyPdfApiRequest.call(this, 'GET', '/list-transactions');
+						const qs: IDataObject = {
+							limit: this.getNodeParameter('limit', i),
+							offset: this.getNodeParameter('offset', i),
+						};
+
+						responseData = await craftMyPdfApiRequest.call(this, 'GET', '/list-transactions', qs);
 
 						returnData.push(responseData as INodeExecutionData);
 					}